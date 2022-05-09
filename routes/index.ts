2a63058f9f40c5999c7ec57fe6781527489b75d5
NicoToff <toffolo01@hotmail.com>
2022-05-09 17:42:37 +0200

    Added prisma

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,18 @@
 import { Router, Request, Response, NextFunction } from "express";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 const router: Router = Router();
 
-router.get("/", (req: Request, res: Response, next: NextFunction) => {
+router.get("/", async (req: Request, res: Response, next: NextFunction) => {
     res.render("index", { title: "TS Express" });
+    const newVisit = await prisma.connection.create({
+        data: {
+            hostname: req.hostname,
+            protocol: req.protocol,
+            method: req.method,
+        },
+    });
+    console.log(newVisit);
 });
 
 export default router;
